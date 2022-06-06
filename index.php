4b68e8b2c06196ac721ea3cf88e9712f000a9e0e
zgs3 <zkairaitis@gmail.com>
2022-06-06 20:50:03 +0300

    Added Login/logout function and function showing file size. Updated Table styling

--- index.php
+++ index.php
@@ -7,6 +7,25 @@ if (isset($_GET['path'])) {
   $currentDir = './';
 }
 
+session_start();
+// logout 
+if (isset($_GET['action']) and $_GET['action'] == 'logout') {
+  session_destroy();
+  session_start();
+}
+
+// login 
+$loginMsg = '';
+if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+  if ($_POST['username'] == 'User' && $_POST['password'] == '12345') {
+    $_SESSION['logged_in'] = true;
+    $_SESSION['timeout'] = time();
+    $_SESSION['username'] = $_POST['username'];
+  } else {
+    $loginMsg = 'Wrong username or password.';
+  }
+}
+
 // Checking file type. File / directory
 function returnFileType($file)
 {
@@ -105,6 +124,18 @@ if (isset($_FILES['uploadedFile'])) {
   header("Location: " . $_SERVER['REQUEST_URI']);
 }
 
+// returning file size in mb
+function returnFileSize($dir, $file)
+{
+  if (is_file($dir . $file) && $file !== 'index.php' && $file !== 'styles.css') {
+    $sizeInMB = (filesize($dir . $file)) / 1000000;
+    return (number_format($sizeInMB, 2) . ' MB'
+    );
+  } else {
+    return (' - - - - - - - ');
+  }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -122,7 +153,42 @@ if (isset($_FILES['uploadedFile'])) {
 
 <body>
 
-  <div id="mainContainer">
+  <header <?php isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true
+            ? print("style = \"display: flex\"")
+            : print("style = \"display: none\"") ?>>
+    <div>
+      <?php
+      if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
+        print('<h2>' . 'Welcome back, ' . $_SESSION['username'] . '!' . '</h2>');
+      } else {
+        NULL;
+      } ?>
+    </div>
+    <div>
+      <button>
+        <a href="index.php?action=logout"> Logout</a>
+      </button>
+    </div>
+  </header>
+
+  <!-- Login form -->
+  <div id="loginForm" <?php isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true
+                        ? print("style = \"display: none\"")
+                        : print("style = \"display: block\"") ?>>
+    <h2>Enter Username and Password</h2>
+    <form action="" method="post" <?php isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true
+                                    ? print("style = \"display: none\"")
+                                    : print("style = \"display: block\"") ?>>
+      <input type="text" name="username" placeholder="username = User" required autofocus></br>
+      <input type="password" name="password" placeholder="password = 12345" required><br>
+      <button type="submit" name="login">Login</button>
+      <h4><?php echo ($loginMsg); ?></h4>
+    </form>
+  </div>
+
+  <div id="mainContainer" <?php isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true
+                            ? print("style = \"display: block\"")
+                            : print("style = \"display: none\"") ?>>
 
     <?php
     // SHOW CURRENT PATH ---------- ---------- ---------- ----------
@@ -143,16 +209,17 @@ if (isset($_FILES['uploadedFile'])) {
         <tr class="tableHeaderRow">
           <th>File Type</th>
           <th>File Name</th>
+          <th>File Size</th>
           <th>Options</th>
         </tr>
         <?php
-        // TABLE DISPLAYING FILES ---------- ---------- ---------- ----------
         $currentFiles = array_values(array_diff(scandir($currentDir), array('.', '..')));
         for ($i = 0; $i < count($currentFiles); $i++) {
           echo ("
               <tr>
                 <td>" . returnFileType($currentDir . $currentFiles[$i]) . "</td>
                 <td>" . returnFile($currentDir, $currentFiles[$i]) . "</td>
+                <td>" . returnFileSize($currentDir, $currentFiles[$i]) . "</td>
                 <td class='actionsBlock'>" . returnDeleteBtn($currentDir, $currentFiles[$i]) . returnDownloadBtn($currentDir, $currentFiles[$i]) . "</td>
               </tr>"
           );
@@ -161,7 +228,9 @@ if (isset($_FILES['uploadedFile'])) {
     </div>
   </div>
 
-  <footer>
+  <footer <?php isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true
+            ? print("style = \"display: flex\"")
+            : print("style = \"display: none\"") ?>>
     <!-- Creating new file -->
     <div id="createBlock">
       <h3>Create new file</h3>
@@ -171,7 +240,7 @@ if (isset($_FILES['uploadedFile'])) {
         <input type="submit">
       </form>
     </div>
-    
+
     <div id="uploadBlock">
       <h3>Upload new file</h3>
       <form action="" method="POST" enctype="multipart/form-data">
